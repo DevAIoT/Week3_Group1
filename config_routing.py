01b3512f9bf7d423cbfed01f91206fb42409e85d
Xskullibur <xskulliburdev@gmail.com>
2026-01-26 14:49:14 +0800

    update ip

--- config_routing.py
+++ config_routing.py
@@ -11,8 +11,8 @@ FOG_COMPLEXITY_THRESHOLD = 5000.0   # Fog handles moderately complex images
 # To find IP address on each laptop:
 #   Windows: ipconfig
 #   Linux/Mac: ifconfig
-FOG_SERVER_URL = "http://172.20.10.4:8000/predict"    # Laptop 2 IP
-CLOUD_SERVER_URL = "http://172.20.10.5:8001/predict"  # Laptop 3 IP
+FOG_SERVER_URL = "http://172.20.10.2:8000/predict"    # Laptop 2 IP
+CLOUD_SERVER_URL = "http://172.20.10.4:8001/predict"  # Laptop 3 IP
 
 # Server Ports
 FOG_SERVER_PORT = 8000
